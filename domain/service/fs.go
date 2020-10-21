b4808d3a8946f00422acd25decab6c06adaf64a8
tighug <kapuriko123@gmail.com>
2020-10-22 03:44:24 +0900

    feat: Change file system libraly to afero

--- domain/service/fs.go
+++ domain/service/fs.go
@@ -2,16 +2,34 @@ package service
 
 import (
 	"os"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/afero"
 )
 
+// AFs ...
+var AFs = &afero.Afero{Fs: afero.NewOsFs()}
+
 // EnsureDir ...
-func EnsureDir(path string) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		os.Mkdir(path, os.ModePerm)
+func EnsureDir(path string) error {
+	if exists, err := AFs.DirExists(path); err != nil {
+		return err
+	} else if !exists {
+		defer log.Info().Msgf("Created an empty directory named %q.", path)
+		return AFs.MkdirAll(path, os.ModePerm)
 	}
+	log.Warn().Msgf("%q already exists.", path)
+	return nil
 }
 
 // EnsureFile ...
-func EnsureFile(path string) {
-	os.OpenFile(path, os.O_RDONLY|os.O_CREATE, os.ModePerm)
+func EnsureFile(path string) (afero.File, error) {
+	if exists, err := AFs.Exists(path); err != nil {
+		return nil, err
+	} else if !exists {
+		defer log.Info().Msgf("Created an empty file named %q.", path)
+		return AFs.Create(path)
+	}
+	log.Warn().Msgf("%q already exists.", path)
+	return nil, nil
 }
